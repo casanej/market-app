30312f8960ffe1316f0794de54722aab82714035
Rafael Casanje <rafael.casanje@gmail.com>
2024-04-04 22:28:03 -0300

    fix: adjusting route file

--- apps/frontend/src/pages/index.tsx
+++ apps/frontend/src/pages/index.tsx
@@ -1,6 +1,6 @@
-import { AppRoutes } from "./routes";
+import { RouterProvider } from "react-router-dom";
+import { routes } from "./routes";
 
 export default function AppRouter() {
-  //return <RouterProvider router={routes} />;
-  return <AppRoutes />
+  return <RouterProvider router={routes} />;
 }
--- apps/frontend/src/pages/routes.tsx
+++ apps/frontend/src/pages/routes.tsx
@@ -1,34 +1,25 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import { HomePage } from "./home";
 import { MonthlyListPage } from "./monthly-list";
 import { MonthlyListIdPage } from "./monthly-list/id";
 
-/* export const routes = createBrowserRouter([
+export const routes = createBrowserRouter([
   {
     path: "/",
     element: <HomePage />,
     index: true,
   },
   {
-    path: "monthly-list",
-    element: <MonthlyListPage />,
+    path: "/monthly-list",
     children: [
       {
-        path: "/monthly-list",
-        id: "listId",
+        index: true,
+        element: <MonthlyListPage />,
+      },
+      {
+        path: ":listId",
         element: <MonthlyListIdPage />,
       },
     ],
   }
-]); */
-
-export const AppRoutes = () => {
-  return <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/monthly-list" element={<MonthlyListPage />}>
-        <Route path=":listId" element={<MonthlyListIdPage />} />
-      </Route>
-    </Routes>
-  </BrowserRouter>
-}
+]);
